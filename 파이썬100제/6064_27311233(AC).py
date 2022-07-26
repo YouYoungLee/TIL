87c81de37297bfb6b378b8fc1962d745c86675b9
YouYoungLee <dldbdud111@gmail.com>
2022-07-26 17:26:05 +0900

    lyy

---
+++ 파이썬100제/6064_27311233(AC).py
@@ -0,0 +1,2 @@
+﻿a,b,c=map(int,input().split())
+print((b if a>b else a) if ((b if a>b else a)<c) else c)
---
+++ 파이썬100제/6066_27311267(AC).py
@@ -0,0 +1,13 @@
+﻿a,b,c=map(int,input().split())
+if a%2==0 :  #논리적으로 한 단위로 처리해야하는 경우 콜론(:)을 찍고, 들여쓰기로 작성 한다.
+    print('even')
+else:
+    print('odd')
+if b%2==0 :  #논리적으로 한 단위로 처리해야하는 경우 콜론(:)을 찍고, 들여쓰기로 작성 한다.
+    print('even')
+else:
+    print('odd')
+if c%2==0 :  #논리적으로 한 단위로 처리해야하는 경우 콜론(:)을 찍고, 들여쓰기로 작성 한다.
+    print('even')
+else:
+    print('odd')
