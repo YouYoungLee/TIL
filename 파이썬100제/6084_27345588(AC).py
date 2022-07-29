6575cdcfeb5f7b5dcdab07b3d97935cd29ed29c0
YouYoungLee <dldbdud111@gmail.com>
2022-07-29 17:03:55 +0900

    lyy

---
+++ 파이썬100제/6084_27345588(AC).py
@@ -0,0 +1,4 @@
+﻿a,b,c,d=map(int,input().split())
+e=(a*b*c*d/8/1024/1024)
+e=round(e,1)
+print(f'{e} MB')
---
+++ 파이썬100제/6085_27345642(AC).py
@@ -0,0 +1,4 @@
+﻿a,b,c=map(int,input().split())
+e=(a*b*c/8/1024/1024)
+
+print(f'{e:.2f} MB')
