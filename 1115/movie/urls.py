d5eae941909412d085f76d884288b00b76acb498
YouYoungLee <dldbdud111@gmail.com>
2022-11-17 16:07:29 +0900

    lyy

--- 1115/movie/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('call/',views.callmovie),
-    path('movielist/',views.movie_list),    
-    path('movie_detail/<int:movie_pk>/',views.movie_detail)
-    
-]
